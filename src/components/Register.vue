89fb34166f69b3de81e7d89d97abaf290e27460a
huseyinkural <huseyinkural35@gmail.com>
2021-01-19 23:47:53 +0300

    token based auth is implemented using with axios

---
+++ src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h4>Register</h4>
+    <form @submit.prevent="register">
+      <label for="name">Username</label>
+      <div>
+          <input id="name" type="text" v-model="username" required autofocus>
+      </div>
+      
+      <label for="password">Password</label>
+      <div>
+          <input id="password" type="password" v-model="password" required>
+      </div>
+      
+      <div>
+          <button type="submit">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        username : "",        
+        password : ""        
+      }
+    },
+    methods: {
+      register: function () {
+        let data = {
+          username: this.username,          
+          password: this.password,   
+        }
+        this.$store.dispatch('register', data)
+       .then(() => this.$router.push('/'))
+       .catch(err => console.log(err))
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,59 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
+import store from '../store'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Login from '../components/Login.vue'
+import Secure from '../components/Secure.vue'
+import Register from '../components/Register.vue'
 
-Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(Router)
 
-const router = new VueRouter({
+let router = new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/secure',
+      name: 'secure',
+      component: Secure,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
